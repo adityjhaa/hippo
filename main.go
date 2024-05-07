14e013d61321ed92bdebb05a5ab308302dc8a855
adityjha0 <aditya2005aps@gmail.com>
2024-05-07 12:27:47 +0530

    added the repl

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
+// main.go
 package main
 
 import (
 	"fmt"
+	"hippo/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Printf("Hello")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Hippo programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,27 @@
+// repl/repl.go
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"hippo/lexer"
+	"hippo/token"
+	"io"
+)
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(">> ")
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
