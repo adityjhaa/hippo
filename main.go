6fc8234228cbb67ff3009a4dc46db1c9b01d6aab
adityjha0 <aditya2005aps@gmail.com>
2024-05-08 14:21:10 +0530

    added new files

--- main.go
+++ main.go
@@ -13,8 +13,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Hello %s! This is the Hippo programming language!\n",
-		user.Username)
+	fmt.Printf("Hello %s! This is the Hippo programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
