e4033e7bde5befc2c066f4052d38f471e7326c7f
adityjhaa <cs1221102@cse.iitd.ac.in>
2024-06-02 10:57:42 +0530

    RRPL => REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"hippo/evaluator"
 	"hippo/lexer"
 	"hippo/parser"
 	"io"
@@ -57,8 +58,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
