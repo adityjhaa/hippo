8887db2ddb97b972dad2b9c4f7ae3f4587e189f7
adityjhaa <cs1221102@cse.iitd.ac.in>
2024-06-03 22:09:58 +0530

    function call & closures

--- object/object.go
+++ object/object.go
@@ -3,7 +3,10 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"hippo/ast"
+	"strings"
 )
 
 type ObjectType string
@@ -19,6 +22,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Integer struct {
@@ -46,6 +50,31 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
 
 type Error struct {
 	Message string
